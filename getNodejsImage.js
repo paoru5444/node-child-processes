9c79d90b25ee43962e03c7c07644427874b0f090
Paulo Roberto <paulo.almeida@wisereducacao.com>
2021-01-05 16:09:23 -0300

    feat: getting node js logo, converted to svg, download and using the execFile method to access the downloaded image

---
+++ getNodejsImage.js
@@ -0,0 +1,14 @@
+const { execFile } = require('child_process');
+
+execFile(__dirname + '/processNodejsImage.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+  }
+
+  console.log(`stdout:\n${stdout}`);
+})
